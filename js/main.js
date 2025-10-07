b94b98a44fb1e85798825651b5eb793d7b0f6f5a
Adam <amatara@gmail.com>
2025-10-07 18:51:11 +1300

    initial

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// ;(function () {
+
+    // 'use strict';
+
+    // var wowAnimation = function() {
+        // var wow = new WOW(
+            // {
+                // animateClass: 'animated',
+                // offset:       150,
+                // callback:     function(box) {
+                    // console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+                // }
+            // }
+        // );
+        // wow.init();
+    // }
+
+
+    // (function($) {
+        // wowAnimation();
+    // })(jQuery);
+
+
+// }());
+
+
+
+// main.js
+// Currently, no animations are used. Keeping this file for future scripts if needed.
