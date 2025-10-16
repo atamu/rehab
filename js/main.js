255e3f4b140869d2e6aca443d024f8965d79568d
Adam <amatara@gmail.com>
2025-10-17 00:41:00 +1300

    Soften scroll animations and improve mobile behaviour

--- js/main.js
+++ js/main.js
@@ -3,16 +3,28 @@
     'use strict';
 
     var wowAnimation = function() {
+        var getOffset = function() {
+            return window.innerWidth <= 768 ? 60 : 120;
+        };
+
         var wow = new WOW(
             {
                 animateClass: 'animated',
-                offset:       150,
+                offset:       getOffset(),
+                mobile:       true,
+                live:         true,
                 callback:     function(box) {
-                    console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+                    console.log("WOW: animating <" + box.tagName.toLowerCase() + ">");
                 }
             }
         );
+
         wow.init();
+
+        window.addEventListener('resize', function() {
+            wow.config.offset = getOffset();
+            wow.sync();
+        });
     }
 
 
